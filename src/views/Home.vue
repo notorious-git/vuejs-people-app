d757c0f1df711bed517bd41cf666a3ee7f77b437
Peter Jang <peter.x.jang@gmail.com>
2018-11-18 09:23:48 -0600

    Display array of people objects

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <h1>Interesting People</h1>
+    <div v-for="person in people">
+      <h2>{{ person.name }}</h2>
+      <p>{{ person.bio }}</p>
+    </div>
   </div>
 </template>
 
@@ -11,7 +15,32 @@
 export default {
   data: function() {
     return {
-      message: "Welcome to Vue.js!"
+      people: [
+        {
+          name: "Bob",
+          bio:
+            "Small batch salvia Marfa chillwave delectus, odio forage art party laborum street art minim fixie locavore hoodie mollit.",
+          bioVisible: true
+        },
+        {
+          name: "Alice",
+          bio:
+            "Tattooed letterpress gluten-free ugh, adipisicing scenester church-key gentrify tousled gastropub pour-over Shoreditch asymmetrical lomo High Life.",
+          bioVisible: true
+        },
+        {
+          name: "Tibor",
+          bio:
+            "Incididunt photo booth ethical reprehenderit adipisicing. Echo Park readymade Bushwick distillery Tonx. +1 semiotics qui duis literally.",
+          bioVisible: true
+        },
+        {
+          name: "Živa",
+          bio:
+            "Excepteur shabby chic semiotics Marfa, quinoa try-hard polaroid pariatur banh mi selfies incididunt brunch trust fund. Ethical dolor PBR&B Tumblr.",
+          bioVisible: true
+        }
+      ]
     };
   },
   created: function() {},
